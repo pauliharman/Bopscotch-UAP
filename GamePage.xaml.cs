e066b78fe3d1b0a7cde30e4fbd2da9b5422a877e
Paul Ian Harman <paulh@ledaentertainment.com>
2015-12-29 17:01:23 +0000

    Added Win10 UAP project files

---
+++ GamePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Bopscotch;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Bopscotch_UAP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class GamePage : Page
+    {
+		readonly Game1 _game;
+
+		public GamePage()
+        {
+            this.InitializeComponent();
+
+			// Create the game.
+			var launchArguments = string.Empty;
+            _game = MonoGame.Framework.XamlGame<Game1>.Create(launchArguments, Window.Current.CoreWindow, swapChainPanel);
+        }
+    }
+}
